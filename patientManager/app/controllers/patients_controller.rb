c2f547fea725723e692d083085a6564db356e85d
lukeyangskywalker <lukeyangskywalker@outlook.com>
2016-06-22 23:12:50 -0400

    added show detail page

--- patientManager/app/controllers/patients_controller.rb
+++ patientManager/app/controllers/patients_controller.rb
@@ -1,5 +1,7 @@
+# controller for the patient
 class PatientsController < ApplicationController
-
+  
+  # standard CRUD for patients
   def index
     @patients = Patient.all
   end
@@ -18,9 +20,9 @@ class PatientsController < ApplicationController
 
   def create
     @patient = Patient.new(patient_params)
-
+    # if save failed, go back to the add new patient page and print error message
     if @patient.save
-     redirect_to @patient
+     redirect_to patients_path
     else
      render 'new'
     end
@@ -28,12 +30,24 @@ class PatientsController < ApplicationController
 
   def update
     @patient = Patient.find(params[:id])
-
+    # if update failed, go back to edit page and print error message
     if @patient.update(patient_params)
-     redirect_to @patient
+     redirect_to patients_path
     else
      render 'edit'
     end
   end
 
+  def destroy
+      @patient = Patient.find(params[:id])
+      # TODO: print error message when destroy fail
+      @patient.destroy
+      redirect_to patients_path
+  end
+
+  # need premit to create new patient
+  private
+    def patient_params
+      params.require(:patient).permit(:firstName, :middleName, :lastName, :weight, :Height, :MRN)
+    end
 end
--- patientManager/app/models/patient.rb
+++ patientManager/app/models/patient.rb
@@ -1,2 +1,9 @@
+# model for patient
 class Patient < ActiveRecord::Base
+  # according to description
+  # firstName, middleName, MRN cannot be null
+  # (I think it should be lastName is required instead of middleName)
+  validates :firstName, presence: true
+  validates :middleName, presence: true
+  validates :MRN, presence: true
 end
